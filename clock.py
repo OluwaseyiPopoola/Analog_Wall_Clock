c25fab6726b3e6a8ad83e2e63752498d5fe73354
Oluwaseyi Popoola <oluwaseyipopoola49@gmail.com>
2025-09-21 18:02:42 -0500

    Add draw_circle to draw circular parts

--- clock.py
+++ clock.py
@@ -4,10 +4,30 @@ import turtle
 def main():
     t = turtle.Turtle()
     t.speed(0)
+
+    # Testing draw_circle method
+    # draw_circle(t, 0.80)
+    # draw_circle(t, 0.70)
+    # turtle.done()
   
 
+def draw_circle(t: turtle.Turtle, scale: int):
+    
+    w = turtle.Screen().window_width()
+    h = turtle.Screen().window_height()
 
+    min_dim = min(w, h)
+    radius = scale*(min_dim // 2) 
 
+    t.penup()
+    t.goto(0, -radius) 
+    t.pendown()
+
+    t.circle(radius) # draws circle
+
+    # returning t to initial position
+    t.penup()
+    t.goto(0,0)
 
 
 if __name__ == "__main__":
